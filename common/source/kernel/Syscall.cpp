aa9ab571e8cfb2598063497741fb51b139b99fa0
Anamarija Momic <a.momic@student.tugraz.at>
2023-10-26 14:23:48 +0200

    fix vpn in fork

--- common/source/kernel/Syscall.cpp
+++ common/source/kernel/Syscall.cpp
@@ -364,7 +364,9 @@ size_t Syscall::pthread_setcanceltype(size_t type, size_t *oldtype) {
 size_t Syscall::fork()
 {
     debug(SYSCALL, "Syscall::fork\n");
-    return ProcessRegistry::instance()->fork();
+    ProcessRegistry::instance()->fork();
+
+    return 0;
 }
 
 size_t Syscall::execv([[maybe_unused]]size_t path, [[maybe_unused]]size_t argv)
