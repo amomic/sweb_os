427b302fc1f47cc5457e4fdccd4158e01b68b023
Anamarija Momic <a.momic@student.tugraz.at>
2023-10-27 12:19:58 +0200

    minor changes in fork

--- userspace/tests/fork1.c
+++ userspace/tests/fork1.c
@@ -11,16 +11,13 @@ int main()
            "Testing Fork\n");
 
     pthread_t* new_thread = 0;
-    pthread_t* new_thread1 = 0;
     int return_value = pthread_create(new_thread, NULL, NULL, NULL);
-    int return_val1ue = pthread_create(new_thread1, NULL, NULL, NULL);
     assert(return_value != 0);
-    assert(return_val1ue != 0);
 
     printf("Starting fork!");
     int retval = fork();
 
-    assert(retval == 0);
+    printf("retval %d\n", retval);
 
     printf("fork1  finished successful!\n");
 
