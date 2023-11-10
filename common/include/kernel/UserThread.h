71840224febf3513ec54cd0ce38c3d4ad23b7908
Mirza Camic <camic@student.tugraz.at>
2023-11-10 20:07:28 +0100

    trying to fix exec

--- common/include/kernel/UserThread.h
+++ common/include/kernel/UserThread.h
@@ -14,7 +14,7 @@ public:
 
     //constructor
     UserThread(ustl::string filename, FileSystemInfo *fs_info, uint32 terminal_number, UserProcess *userProcess,
-               void *(*start_routine)(void *), void *wrapper, size_t tid, void *argc, size_t args);
+               void *(*start_routine)(void *), void *wrapper, size_t tid, void* argc, size_t args);
 
     //copy constructor
     UserThread([[maybe_unused]]const UserThread &process_thread_pointer, UserProcess *parent_process,
