65bcedd93f0654d30be5424c287612d458f135b9
Amar Sehovic <asehovic@student.tugraz.at>
2023-11-14 18:36:31 +0100

    merging exit fix

--- common/source/kernel/ProcessRegistry.cpp
+++ common/source/kernel/ProcessRegistry.cpp
@@ -150,12 +150,13 @@ size_t ProcessRegistry::fork()
 void ProcessRegistry::updateExitCode(size_t code)
 {
     process_lock_.acquire();
-    UserThread *current_thread = (UserThread*)currentThread;
-    UserProcess *current_process = current_thread->getProcess();
+    UserThread* current_thread = (UserThread*)currentThread;
+    UserProcess* current_process = current_thread->getProcess();
     process_retval_map_.push_back(ustl::make_pair(current_process->pid_, code));
     current_process->threads_lock_.acquire();
     for( auto it : current_process->threads_map_)
-        it.second->makeAsynchronousCancel();
+        if( it.first != current_thread->tid_)
+            it.second->makeAsynchronousCancel();
     current_process->threads_lock_.release();
     process_lock_.release();
 }
