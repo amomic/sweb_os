8daf1dd6ed16d02a16081707085f5dab8fcc8b9d
Anamarija Momic <a.momic@student.tugraz.at>
2023-11-14 18:22:53 +0100

    test

--- userspace/tests/stack.c
+++ userspace/tests/stack.c
@@ -7,8 +7,8 @@ void* thread_function(void* arg) {
     int value = 1;
     int* val = &value;
     while(1) {
-        //val += 4096;
-        val -= 40;
+        val += 4096;
+        //val -= 40;
         *val = 2;
         printf("Value %d and Pointer %p\n", value, val);
     }
