cf35b3eb5dda84631ffe34881427acc82b80a7b6
Anamarija Momic <a.momic@student.tugraz.at>
2023-12-26 02:35:34 +0100

    change

---
+++ userspace/tests/swap1.c
@@ -0,0 +1,30 @@
+#include "pthread.h"
+#include "stdio.h"
+#include "assert.h"
+#include "unistd.h"
+#define MB 1024ULL * 1024ULL
+
+intptr_t global[7*MB];
+
+int main()
+{
+
+    for(size_t i = 0; i < 7*MB; ++i)
+    {
+        if(!(i % 4096))
+        {
+            global[i] = (intptr_t)&global[i];
+        }
+    }
+
+    for(size_t i = 0; i < 7*MB; ++i)
+    {
+        if(!(i % 4096))
+        {
+            printf(" global id is: %ld, and we got %ld\n", global[i], (intptr_t)&global[i] );
+            assert(global[i] == (intptr_t)&global[i] && "swapIn failed");
+        }
+    }
+
+    return 0;
+}
