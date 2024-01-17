ffde893f766757c176d1738cd93c6cc11fc22be4
Ascic, Maid <maid.ascic@student.tugraz.at>
2024-01-17 23:45:26 +0100

    Reverted due to build fail

--- common/source/mm/PageFaultHandler.cpp
+++ common/source/mm/PageFaultHandler.cpp
@@ -188,6 +188,7 @@ inline void PageFaultHandler::handlePageFault(size_t address, bool user,
 
         if (address > STACK_POS)
         {
+
             debug(SYSCALL, "7\n");
 
             if (((UserThread *) currentThread)->process_->CheckStack(address, &pages))
