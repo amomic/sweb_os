3b9af23ab1b13a4354983ee16bb1cdec69e0d228
Ascic, Maid <maid.ascic@student.tugraz.at>
2024-01-19 10:42:06 +0100

    Typo fix

--- userspace/tests/brk.c
+++ userspace/tests/brk.c
@@ -6,18 +6,14 @@
 
 int main()
 {
-    //printf("%p\n", brk(2));
-    // 134250496
-    // 134250555
+
     int a = brk((void *) 0x00007D0000000000ULL);
     printf("%d\n", a);
     void *b = sbrk(2);
     printf("%p\n", b);
     void *c = sbrk(0);
-
     printf("%p\n", c);
     void *d = sbrk(2);
-
     printf("%p\n", d);
 
     return 0;
